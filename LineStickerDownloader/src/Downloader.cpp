ac84a91002297856d6da4e0dea5554c6923f6f4b
Kashi66 <arkashi66@gmail.com>
2018-10-14 14:21:31 +0200

    fix

--- LineStickerDownloader/src/Downloader.cpp
+++ LineStickerDownloader/src/Downloader.cpp
@@ -14,7 +14,8 @@
 
 namespace
 {
-	static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+	static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) 
+	{
 		((std::string*)userp)->append((char*)contents, size * nmemb);
 		return size * nmemb;
 	}
